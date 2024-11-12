650c5c240ecc576146280f821bd6302e1897ecd1
CSman16 <cadesmith538@gmail.com>
2024-11-12 10:42:51 -0500

    Update Uno.py

--- Uno.py
+++ Uno.py
@@ -7,8 +7,8 @@ if game == 1:
     #intro
     print('''
         welcome to Uno! its like uno but in python and your arch nemesis this time is Python yet again. 
-        the rules that are allowed in this game is stacking and that you can only draw one card before your turn is over. 
-        you start with seven cards and the first participant with no cards wins.
+        the rules that are allowed in this game is stacking and that you can only draw one card before
+        your turn is over. you start with seven cards and the first participant with no cards wins.
     ''')
 
 num_players = 2
@@ -47,8 +47,12 @@ def deal_cards(uno_deck,num_cards):
     player_hand = random.sample(uno_deck,num_cards)
     for card in player_hand:
         uno_deck.remove(card)
-        print(player_hand)
-        return uno_deck, player_hand
 
-deal_cards(uno_deck, num_cards)
+    python_hand = random.sample(uno_deck, num_cards)
 
+    for card in python_hand:
+        uno_deck.remove(card)
+    return uno_deck, player_hand, python_hand
+
+uno_deck, player_hand, python_hand = deal_cards(uno_deck, num_cards)
+print("your hand:", player_hand)
