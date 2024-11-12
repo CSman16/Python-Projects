b5c09ca4b75e870e43f28f331c94ba08c7941d8f
CSman16 <cadesmith538@gmail.com>
2024-11-12 10:29:26 -0500

    Player Hand dealt

--- Uno.py
+++ Uno.py
@@ -8,35 +8,47 @@ if game == 1:
     print('''
         welcome to Uno! its like uno but in python and your arch nemesis this time is Python yet again. 
         the rules that are allowed in this game is stacking and that you can only draw one card before your turn is over. 
-        you start with seven cards amd the first participant with no cards wins.
+        you start with seven cards and the first participant with no cards wins.
     ''')
 
 num_players = 2
-
+uno_deck = []
 
 time.sleep(10)
-def deck():
+def deck(uno_deck):
     uno_deck = []
     colors = ["blue","red","yellow","green"]
     numbers = [0,1,2,3,4,5,6,7,8,9]
-    action_cards = ["Skip", "Draw 2", "Reverse", "Wild", "Wild Draw 4"]
+    action_cards = ["Skip", "Draw 2", "Reverse"]
     for color in colors:
         for number in numbers:
             card = (color, number)
             uno_deck.append(card)
-
-deck()
-
+    for color in colors:
+        for action in action_cards:
+            card = (color, action)
+            uno_deck.append(card)
+            uno_deck.append(card)
+    for _ in range(4):
+        uno_deck.append(("Wild", "Wild"))
+        uno_deck.append(("Wild", "Wild Draw 4"))
+    return uno_deck
+deck(uno_deck)
+uno_deck = deck(uno_deck)
 def shuffle(uno_deck):
     random.shuffle(uno_deck)
     return uno_deck
 
 shuffle(uno_deck)
 
-def deal_cards(deck,num_cards):
-    player_hand = random.sample(deck,num_cards)
-    for card in player_hand:
-        deck.remove(card)
+num_cards = 7
 
+def deal_cards(uno_deck,num_cards):
+    player_hand = random.sample(uno_deck,num_cards)
+    for card in player_hand:
+        uno_deck.remove(card)
+        print(player_hand)
         return uno_deck, player_hand
-        
+
+deal_cards(uno_deck, num_cards)
+
